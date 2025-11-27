ec691c9751f3085a51b6873a023b4694d66bef5c
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-11-27 17:41:02 -0300

    feat: add routes into api routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\TurmaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/hello', function() {
-    return ["message" => "Hello World"];
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/teachers', [TeacherController::class, 'index'])->name('index');
+    Route::post('/teachers', [TeacherController::class, 'store'])->name('store');
+    Route::patch('/teachers/{teacher}', [TeacherController::class, 'update'])->name('update');
+    Route::delete('/teachers/{teacher}', [TeacherController::class, 'destroy'])->name('destroy');
+
+    Route::get('/students', [StudentController::class, 'index'])->name('index');
+    Route::post('/students', [StudentController::class, 'store'])->name('store');
+    Route::put('/students/{students}', [StudentController::class, 'update'])->name('update');
+    Route::delete('/students/{students}', [StudentController::class, 'destroy'])->name('destroy');
+
+    Route::get('/turmas', [TurmaController::class, 'index'])->name('index');
+    Route::post('/turmas', [TurmaController::class, 'store'])->name('store');
+    Route::patch('/turmas/{turmas}', [TurmaController::class, 'update'])->name('update');
+    Route::delete('/turmas/{turmas}', [TurmaController::class, 'destroy'])->name('destroy');
+
 });
-
-Route::get('/student', [StudentController::class, 'index'])->name('student.index');
-Route::post('/student', [StudentController::class, 'show'])->name('student.show');
