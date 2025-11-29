aad0fc7ed7f3d71d1442386ac0f3f8e70e35c702
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-11-29 09:19:47 -0300

    reafctor: add route to Register auth bearer token

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\TurmaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
@@ -19,13 +19,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/students', [StudentController::class, 'index'])->name('index');
     Route::post('/students', [StudentController::class, 'store'])->name('store');
-    Route::put('/students/{students}', [StudentController::class, 'update'])->name('update');
-    Route::delete('/students/{students}', [StudentController::class, 'destroy'])->name('destroy');
+    Route::patch('/students/{student}', [StudentController::class, 'update'])->name('update');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('destroy');
 
     Route::get('/turmas', [TurmaController::class, 'index'])->name('index');
     Route::post('/turmas', [TurmaController::class, 'store'])->name('store');
-    Route::patch('/turmas/{turmas}', [TurmaController::class, 'update'])->name('update');
-    Route::delete('/turmas/{turmas}', [TurmaController::class, 'destroy'])->name('destroy');
-    Route::post('/turmas/{turmas}/professor', [TurmaController::class, 'addTeacher'])->name('addTeacher');
-
+    Route::patch('/turmas/{turma}', [TurmaController::class, 'update'])->name('update');
+    Route::delete('/turmas/{turma}', [TurmaController::class, 'destroy'])->name('destroy');
+    Route::post('/turmas/{turma}/professor', [TurmaController::class, 'addTeacher'])->name('addTeacher');
+    Route::get('/turmas/{turma}', [TurmaController::class, 'show'])->name('show');
 });
